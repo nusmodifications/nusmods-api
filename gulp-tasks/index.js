192bab17ce9331602f7670dc888d20ad508ca430
Li Kai <falconets25601@gmail.com>
2017-04-28 16:49:55 +0800

    Export smu and ntu

--- gulp-tasks/index.js
+++ gulp-tasks/index.js
@@ -5,11 +5,15 @@ import examTimetable from './remote/examTimetable';
 import ivle from './remote/ivle';
 import moduleTimetableDelta from './remote/moduleTimetableDelta';
 import venues from './remote/venues';
+import smu from './remote/smu';
+import ntuDetails from './remote/ntuDetails';
+import ntuLessons from './remote/ntuLessons';
 import mergeCorsBiddingStats from './local/mergeCorsBiddingStats';
 import consolidateForSem from './local/consolidateForSem';
 import consolidateForYear from './local/consolidateForYear';
 import splitForSem from './local/splitForSem';
 import splitForYear from './local/splitForYear';
+import parseExcel from './local/parseExcel';
 
 export default {
   bulletinModules,
@@ -19,9 +23,13 @@ export default {
   ivle,
   moduleTimetableDelta,
   venues,
+  smu,
+  ntuDetails,
+  ntuLessons,
   mergeCorsBiddingStats,
   consolidateForSem,
   splitForSem,
   consolidateForYear,
   splitForYear,
+  parseExcel,
 };
